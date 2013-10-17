e90fcac6c5795481e0f9eb2a67cc0c6c255300c9
Ignacio Inglese <inglese.ignacio@gmail.com>
2013-10-17 14:20:50 -0300

    Added JSON RPC response serializer

---
+++ AFJSONRPCClient/AFJSONRPCResponseSerializer.h
@@ -0,0 +1,13 @@
+//
+//  AFJSONRPCResponseSerializer.h
+//
+//
+//  Created by Ignacio Inglese on 10/17/13.
+//  Copyright (c) 2013 Ignacio Inglese Inc. All rights reserved.
+//
+
+#import "AFURLResponseSerialization.h"
+
+@interface AFJSONRPCResponseSerializer : AFJSONResponseSerializer
+
+@end
---
+++ AFJSONRPCClient/AFJSONRPCResponseSerializer.m
@@ -0,0 +1,22 @@
+//
+//  AFJSONRPCResponseSerializer.m
+//
+//
+//  Created by Ignacio Inglese on 10/17/13.
+//  Copyright (c) 2013 Ignacio Inglese Inc. All rights reserved.
+//
+
+#import "AFJSONRPCResponseSerializer.h"
+
+@implementation AFJSONRPCResponseSerializer
+
+- (instancetype)init {
+    if (self = [super init]) {
+        NSMutableSet * mutableContentTypes = [NSMutableSet setWithSet:self.acceptableContentTypes];
+        [mutableContentTypes addObject:@"application/json-rpc"];
+        self.acceptableContentTypes = [NSSet setWithSet:mutableContentTypes];
+    }
+    return self;
+}
+
+@end
